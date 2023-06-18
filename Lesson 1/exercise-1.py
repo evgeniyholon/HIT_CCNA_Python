79323825552c61738970b657eceef8ea8e3e92bf
evgeniyholon <evgeniyholon@gmail.com>
2023-06-18 13:34:19 +0300

    Add empty lines

--- Lesson 1/exercise-1.py
+++ Lesson 1/exercise-1.py
@@ -1,5 +1,7 @@
 doron = 3
 liran = 5
 elay = 6
+
 Total_Apples = doron + liran + elay
+
 print(f'Doron - {doron} apples\nLiran - {liran} apples\nElay - {elay} apples\nTotal - {Total_Apples} apples')
