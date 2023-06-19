dfe445b260a3523eede081c63a55c44f528cb9a7
evgeniyholon <evgeniyholon@gmail.com>
2023-06-19 14:53:40 +0300

    format

--- Lesson 1/exercise-1.py
+++ Lesson 1/exercise-1.py
@@ -4,4 +4,7 @@ elay = 8
 
 Total_Apples = doron + liran + elay
 
-print(f'Doron - {doron} apples\nLiran - {liran} apples\nElay - {elay} apples\nTotal - {Total_Apples} apples')
+print(
+    f'Doron - {doron} apples\nLiran - {liran} apples\n'
+    f'Elay - {elay} apples\nTotal - {Total_Apples} apples'
+)
